f30f87bbef5e7e8f411b6c39a4139d320feb28aa
Ignat Insarov <kindaro@gmail.com>
2020-03-28 15:01:56 +0300

    Put examples in their own file.

--- grammar.ml
+++ grammar.ml
@@ -30,27 +30,3 @@ This way to define a grammar is isomorphic to the traditional quadruple ⟨non-t
 The sets of terminals and intermediaries can be derived by scanning the rule set. The start symbol is defined by the dedicated context-free rule group. The rule should then be added to the other rules.
 
 *)
-
-let cat_intermedial = Intermedial "cat"
-
-let cat_terminal = Terminal "purr"
-
-let cat_rule_group: (string, string) context_free_rule_group =
-  { source = cat_intermedial
-  ; target =
-      [ [Terminary cat_terminal]
-      ; [ Intermediary cat_intermedial
-        ; Intermediary cat_intermedial ] ] }
-
-let cat_grammar =
-  let cat = cat_intermedial
-  and purr = Terminary cat_terminal
-  and meow = Terminary (Terminal "meow")
-  in
-  { rules = [ ]
-  ; start =
-      { source = cat
-      ; target =
-          [ [purr]
-          ; [meow]
-          ; [Intermediary cat; Intermediary cat] ] } }
