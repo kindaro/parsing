e9b8f243f8a35b11cff9a7b3d07909a3ae6f58c2
Ignat Insarov <kindaro@gmail.com>
2020-04-05 20:19:56 +0300

    More work on Unicode helpers.

--- unicode.ml
+++ unicode.ml
@@ -8,14 +8,33 @@ let string_to_chars (s: string): Uchar.t list =
        match Uutf.decode decoder with
        | `Uchar u -> decode_string (u :: us)
        | `End -> us
-       | _ -> failwith "Some other constructor."
-  in decode_string [ ]
+       | `Await -> failwith "Await is only emitted with Manual source."
+       | `Malformed _ -> decode_string (Uutf.u_rep :: us)
+  in List.rev (decode_string [ ])
+
+let transcode_chars normalization_form (us: Uchar.t list): Uchar.t list =
+  let transcoder = Uunf.create normalization_form
+  in let rec roundtrip' (ws: Uchar.t list) (x: Uunf.ret): Uchar.t list =
+       match Uunf.add transcoder x with
+       | `Await | `End -> ws
+       | `Uchar w -> roundtrip' (w :: ws) `Await
+  in let roundtrip = roundtrip' [ ]
+  in let rec transcode' (ws: Uchar.t list) (us: Uchar.t list): Uchar.t list =
+       match us with
+       | [ ] -> let ys = roundtrip `End in List.append ys ws
+       | u :: us -> let ys = roundtrip (`Uchar u) in transcode' (List.append ys ws) us
+  in let transcode = transcode' [ ]
+  in List.rev (transcode us)
 
 let chars_to_string (us: Uchar.t list): string =
   let temporary_buffer = Buffer.create 16
   in let encoder = Uutf.encoder (`UTF_8) (`Buffer temporary_buffer)
+  in let push_one (x): unit =
+       match Uutf.encode encoder x with
+         | `Ok -> ()
+         | `Partial -> failwith "Partial is only returned with Manual destination."
   in let rec encode_chars (chars: Uchar.t list): unit =
        match chars with
-       | [ ] -> let _ = Uutf.encode encoder `End in ()
-       | (u :: us) -> let _ = Uutf.encode encoder (`Uchar u) in encode_chars us
-  in let () = encode_chars (List.rev us) in Buffer.contents temporary_buffer
+       | (u :: us) -> let () = push_one (`Uchar u) in encode_chars us
+       | [ ] -> let () = push_one `End in ()
+  in let () = encode_chars us in Buffer.contents temporary_buffer
