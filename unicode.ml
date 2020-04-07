c78797d82491b8f9fa6a23100c19e51d51f9354b
Ignat Insarov <kindaro@gmail.com>
2020-04-07 20:01:45 +0300

    Add segmentation into graphemes.

--- unicode.ml
+++ unicode.ml
@@ -26,6 +26,21 @@ let normalize_chars normalization_form (us: Uchar.t list): Uchar.t list =
   in let transcode = transcode' [ ]
   in List.rev (transcode us)
 
+let chars_to_graphemes (us: Uchar.t list): (grapheme list * Uchar.t list) =
+  let transcoder = Uuseg.create `Grapheme_cluster
+  in let rec roundtrip' (graphemes: grapheme list) (remainder: Uchar.t list) (x): (grapheme list * Uchar.t list) =
+       match Uuseg.add transcoder x with
+       | `Await | `End -> (graphemes, remainder)
+       | `Boundary -> roundtrip' (Grapheme (List.rev remainder) :: graphemes) [ ] `Await
+       | `Uchar w -> roundtrip' graphemes (w :: remainder) `Await
+  in let roundtrip = roundtrip' []
+  in let rec transcode' (xs: grapheme list) (remainder: Uchar.t list) (us: Uchar.t list): (grapheme list * Uchar.t list) =
+       match us with
+       | [ ] -> let (ys, remainder) = roundtrip remainder `End in (List.rev (List.append ys xs), List.rev remainder)
+       | u :: us -> let (ys, remainder) = roundtrip remainder (`Uchar u) in transcode' (List.append ys xs) remainder us
+  in let transcode = transcode' [ ] [ ]
+  in transcode us
+
 let chars_to_string (us: Uchar.t list): string =
   let temporary_buffer = Buffer.create 16
   in let encoder = Uutf.encoder (`UTF_8) (`Buffer temporary_buffer)
