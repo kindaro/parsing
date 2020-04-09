e920441a73aac8f72f7f378f24137d41de93f7ad
Ignat Insarov <kindaro@gmail.com>
2020-04-09 21:27:36 +0300

    Add some helpers for quick sentence construction.

---
+++ chars.ml
@@ -0,0 +1,18 @@
+open Core
+open Grammar
+open Unicode
+
+let string_to_sentential_form (s: string): (grapheme, grapheme) symbol list =
+  let convert_to_symbol (x: grapheme): (grapheme, grapheme) symbol =
+    if is_any_upper_case x
+    then Intermediary (Intermedial x)
+    else Terminary (Terminal x)
+  in s |> graphemes |> List.map ~f: convert_to_symbol
+
+let sentential_form_to_string (xs: (grapheme, grapheme) symbol list): string =
+  let convert_from_symbol (x: (grapheme, grapheme) symbol): grapheme = match x with
+    | Intermediary (Intermedial y) -> y
+    | Terminary (Terminal y) -> y
+  in xs |> List.map ~f: convert_from_symbol |> show_graphemes |> String.concat
+
+let sf = string_to_sentential_form
