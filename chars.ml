575ac09115fc06f2e3f66e3df68719c3119700c8
Ignat Insarov <kindaro@gmail.com>
2020-04-10 02:53:33 +0300

    Minor adjustments.

--- chars.ml
+++ chars.ml
@@ -9,10 +9,12 @@ let string_to_sentential_form (s: string): (grapheme, grapheme) symbol list =
     else Terminary (Terminal x)
   in s |> graphemes |> List.map ~f: convert_to_symbol
 
+let sf = string_to_sentential_form
+
 let sentential_form_to_string (xs: (grapheme, grapheme) symbol list): string =
   let convert_from_symbol (x: (grapheme, grapheme) symbol): grapheme = match x with
     | Intermediary (Intermedial y) -> y
     | Terminary (Terminal y) -> y
-  in xs |> List.map ~f: convert_from_symbol |> show_graphemes |> String.concat
+  in xs |> List.map ~f: convert_from_symbol |> List.map ~f: grapheme_to_string |> String.concat
 
-let sf = string_to_sentential_form
+let fs = sentential_form_to_string
