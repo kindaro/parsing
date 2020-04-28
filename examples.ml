5b38a0fdd276f9aab3cfad5f7ff993214a34a3d2
Ignat Insarov <kindaro@gmail.com>
2020-04-28 21:10:15 +0300

    Use beautiful words for example.

--- examples.ml
+++ examples.ml
@@ -2,7 +2,7 @@ open Grammar
 open Chars
 open Unicode
 
-let cat_graphemes = sf "fence CAT house CAT CAT garage"
+let cat_graphemes = sf "fence CAT house CAT CAT barn"
 
 let more_cats = rule "CAT" "CAT CAT"
 
@@ -10,4 +10,5 @@ let cats_purr = rule "CAT CAT" "(two cats purr)"
 
 let cat_meows = rule "CAT" "(cat meows)"
 
-let cat_grammar: (grapheme, grapheme) grammar = {start = cf_rules "S" ["CAT"]; rules = [more_cats; cats_purr; cat_meows]}
+let cat_grammar: (grapheme, grapheme) grammar =
+  {start = {source = Intermedial (char "S"); target = [cat_graphemes]}; rules = [more_cats; cats_purr; cat_meows]}
